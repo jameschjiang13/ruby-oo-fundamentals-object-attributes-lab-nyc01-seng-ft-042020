675b3cbe791fce00773dc99c196f4c2d25682bea
jameschjiang13 <monochrome473@gmail.com>
2020-04-21 16:26:36 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,28 @@
+require 'pry'
+# class Person
+#     def name=(name)
+#         @name = name
+#     end
+
+#     def name
+#         @name
+#     end
+
+#     def job=(job)
+#         @job = job
+#     end
+
+#     def job
+#         @job
+#     end
+
+# end
+
+class Person
+    attr_accessor :name, :job
+    # def initialize(name = nil, job = nil)
+    #     @name = name
+    #     @job = job
+    # end
+
+end
